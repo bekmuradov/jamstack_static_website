fbe768e4d43b1072fffab6f708f22d730acee9d1
Beck <47065940+bekmuradov@users.noreply.github.com>
2020-11-05 00:52:56 +0700

    descriptive env var

--- src/_data/cars.js
+++ src/_data/cars.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 module.exports = async () => {
     try {
-        const res = await axios.get(`https://www.fake-api.com/api/${process.env.API_ENDPOINT}`)
+        const res = await axios.get(`https://www.fake-api.com/api/${process.env.CARS_API_ENDPOINT}`)
         return res.data.response.data.cars
     } catch (error) {
         console.error(error);
